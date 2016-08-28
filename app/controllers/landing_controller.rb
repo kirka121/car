d8bb79c5f3212666417ac23b272b70980db8572d
Kirka <kirka121@gmail.com>
2016-08-27 20:14:43 -0400

    toggle code

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,6 +1,28 @@
 class LandingController < ApplicationController
+  include PiPiper
 
   def index
 
   end
+
+  def toggle_lamp
+    byebug
+
+    lamp = PiPiper::Pin.new(pin: 17, direction: :out)
+
+    if lamp_params == :on
+      lamp.on
+    else
+      lamp.off
+    end
+
+    redirect_to :index
+  end
+
+  private
+
+  def lamp_params
+    params.require(:lamp).permit(:toggle)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root 'landing#index'
+
+  post '/toggle_lamp', to: 'landing#toggle_lamp'
 end
